a1198ff8f95d8fc7705e29af6522c365d5ef4f98
luananama <luana.mariav@gmail.com>
2019-02-11 13:01:23 +0100

    no change

--- algorithms/simulated_annealing.py
+++ algorithms/simulated_annealing.py
@@ -15,7 +15,7 @@ import math
 
 #1. read in the text file with the warehouse information
 warehouse = []
-for line in open("problem1.txt"):
+for line in open("../data/problem1.txt"):
         psu = line.strip().split(" ")
         warehouse.append(psu)
 stock = warehouse[0] #the first line, what is in stock
@@ -24,17 +24,16 @@ warehouse = warehouse[2:] #now we have a list of the psus
 #print(stock)
 
 #2. open the order11, store in list
-for item in open("order11.txt"):
+for item in open("../data/order11.txt"):
     order11 = item.split(" ")
 
 #3. open the order12
-for item in open("order12.txt"):
+for item in open("../data/order12.txt"):
     order12 = item.split(" ")
     #order_new = order12[2:]
 
 # Dictionary items to numbers
 dictionary_stock = {}
-i: int
 for i in range(len(stock)):
     dictionary_stock[stock[i]] = i+1
 #(dictionary_stock)
@@ -44,6 +43,7 @@ def replace_matched_PSU(word_list, dictionary):
     new_list = [[dictionary.get(item, item) for item in lst] for lst in word_list]
     return new_list
 NoWarehouse = replace_matched_PSU(warehouse, dictionary_stock)
+
 #print(NoWarehouse)
 
 # convert the items in the order to Numbers
